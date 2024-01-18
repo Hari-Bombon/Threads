c037511c49506d4dadbafb2b8c2fb1529ae5f318
Haripriya <haripriya14022003@gmail.com>
2024-01-18 19:02:23 +0530

    Added the shacn-ui and themes

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,41 +1,45 @@
 import Link from "next/link";
 import Image from "next/image";
-import { OrganizationProfile, OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
-function Topbar(){
-    const isUserLoggedIn = true;
-    return (
-        <nav className="topbar">
-            <Link href="/" className="flex items-center gap-4">
-            <Image src='/threads_assest/assets/logo.svg' alt="logo" width={28} height={28} />
-                <p className="text-heading3-bold text-light-1 max-xs:hidden">
-                    Threads
-                </p>
-            </Link>
-            <div className="flex items-center gap-1">
-                <div className="block md:hidden">
-                    <SignedIn>
-                  <SignOutButton>
-                        <div className="flex cursor-pointer">
-                            <Image
-                                src='/threads_assest/assets/logout.svg'
-                                alt="logout"
-                                width={24}
-                                height={24}
-                                />
-                        </div>
-                  </SignOutButton>
-                  </SignedIn>
-                </div>
-                <OrganizationSwitcher
-                    appearance={{
-                        elements:{
-                            organizationSwitcherTrigger: "py-2 px-4"
-                        }
-                    }}
+function Topbar() {
+  const isUserLoggedIn = true;
+
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/threads_assest/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/threads_assest/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
                 />
-            </div>
-        </nav>
-    )
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher
+          // Use type assertion to avoid TypeScript error
+          // @ts-ignore
+          baseTheme={dark}
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
 }
+
 export default Topbar;
