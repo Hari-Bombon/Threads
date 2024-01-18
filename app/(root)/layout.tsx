3561720181532a2363d0088600731ce670f43363
Haripriya <haripriya14022003@gmail.com>
2024-01-18 18:12:59 +0530

    added the sign-in/out and created the sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,13 +3,18 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../globals.css'
 
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Bottombar from '@/components/shared/Bottombar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Threads',
-  description: '',
-}
+  description: 'A Next.js Threads Application',
+};
 
 export default function RootLayout({
   children,
@@ -19,7 +24,19 @@ export default function RootLayout({
   return (
 <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Topbar/>
+        <main className='flex flex-row'>
+          <LeftSidebar/>
+          <section className='main-container'>
+            <div className='w-full max-w-4xl'>
+              {children}
+            </div>
+          </section>
+          <RightSidebar/>
+        </main>
+        <Bottombar/>
+        </body>
     </html>
     </ClerkProvider>
   )
---
+++ constants/index.js
@@ -0,0 +1,44 @@
+export const sidebarLinks = [
+    {
+      imgURL: "/threads_assest/assets/home.svg",
+      route: "/",
+      label: "Home",
+    },
+    {
+      imgURL: "threads_assest/assets/search.svg",
+      route: "/search",
+      label: "Search",
+    },
+    {
+      imgURL: "/threads_assest/assets/heart.svg",
+      route: "/activity",
+      label: "Activity",
+    },
+    {
+      imgURL: "/threads_assest/assets/create.svg",
+      route: "/create-thread",
+      label: "Create Thread",
+    },
+    {
+      imgURL: "/threads_assest/assets/community.svg",
+      route: "/communities",
+      label: "Communities",
+    },
+    {
+      imgURL: "/threads_assest/assets/user.svg",
+      route: "/profile",
+      label: "Profile",
+    },
+  ];
+  
+  export const profileTabs = [
+    { value: "threads", label: "Threads", icon: "/threads_assest/assets/reply.svg "},
+    { value: "replies", label: "Replies", icon: "/threads_assest/assets/members.svg" },
+    { value: "tagged", label: "Tagged", icon: "/public/threads_assest/assets/tag.svg" },
+  ];
+  
+  export const communityTabs = [
+    { value: "threads", label: "Threads", icon: "/threads_assest/assets/reply.svg" },
+    { value: "members", label: "Members", icon: "/threads_assest/assets/members.svg" },
+    { value: "requests", label: "Requests", icon: "/threads_assest/assets/request.svg" },
+  ];
