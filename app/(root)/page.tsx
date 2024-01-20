ad544423bc6752428625d6b147bfbcf88f1c526f
Haripriya <haripriya14022003@gmail.com>
2024-01-20 18:08:33 +0530

    Added the thread post fun

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,51 @@
+"use client";
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+import { useState , useEffect } from "react";
+// Import necessary dependencies
+
+// Example Thread Model Definition
+interface Thread {
+  id: string;
+  text: string;
+  author: string;
+  community: string | null;
+  parentId: string | null; // Add this line
+  createdAt: Date;
+  // ... other properties
+}
 
 export default function Home() {
-  return(
+  const [posts, setPosts] = useState([]);
+  const [user, setUser] = useState(null);
+
+
+
+  return (
     <>
-     <h1 className="head-text text-left">Home</h1>
+      <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {posts.length === 0 ? (
+          <p className="no-result">No posts found.</p>
+        ) : (
+          posts.map((post) => (
+            <ThreadCard
+              key={post._id}
+              post={post._id}
+              currentUserId={user?.id}
+              parentId={post.parentId}
+              content={post.text}
+              author={post.author}
+              community={post.community}
+              createdAt={post.createdAt}
+              comments={post.children}
+            />
+          ))
+        )}
+      </section>
     </>
-  )
+  );
 }
+
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -20,7 +20,7 @@ export async function createThread({ text, author, communityId, path }: Params) 
     const createdThread = await Thread.create({
       text,
       author,
-      community: communityId ? communityId : null,
+      community: communityId || null,
     });
 
     if (!createdThread) {
@@ -38,4 +38,30 @@ export async function createThread({ text, author, communityId, path }: Params) 
   } catch (error: any) {
     throw new Error(`Error creating Thread: ${error.message}`);
   }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  const skipAmount = (pageNumber - 1) * pageSize;
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } });
+
+  const posts = await postsQuery.exec();
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
